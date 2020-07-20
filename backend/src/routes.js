b16ef6d2d61866ac94b6badc1c32e3106cf604d3
gxstav <gustavoscbarcelos@gmail.com>
2020-07-19 22:19:37 -0300

    initial setup

---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { celebrate, Joi, Segments } = require('celebrate')
+// const ong = require('./controllers/OngController')
+// const incident = require('./controllers/IncidentController')
+// const profile = require('./controllers/ProfileController')
+// const session = require('./controllers/SessionController')
+const routes = express.Router()
+
+
+
+module.exports = routes
