dd689a272e63e9bc79e04e523a518979133f4690
Augusto <augusto_g_carvalho@estudante.sc.senai.br>
2020-08-31 21:39:04 -0300

    Cadastro2

--- frontend/src/pages/Cadastro/index.jsx
+++ frontend/src/pages/Cadastro/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import Navbar from '../../components/Navbar'
 import Carousel from '../../components/Carousel'
@@ -13,6 +13,7 @@ import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
   wrapperCol: { span: 16 },
 }; 
 
+/*
 const validateMessages = {
   required: '${label} is required!',
   types: {
@@ -23,7 +24,7 @@ const validateMessages = {
     range: '${label} must be between ${min} and ${max}',
   },
 };
-
+ */
 
 //
 // PODE IMPORTAR LOGOS, SVG, COMPONENTES AQUI
@@ -37,24 +38,77 @@ function Cadastro(){
     <div id="cadastroPage">
      
      <Form {...layout} name="cadastro">
-      <Form.Item name={['user', 'name']} label="Nome do Responsável" rules={[{ required: true }]}>
+      <Form.Item name="nome" label="Nome do Responsável" rules={[{ required: true }]}>
+        <Input />    
+        
+        <p></p>
+        <Form.Item
+        name="email" label="Email da ONG ou Responsável" rules={[{ type: 'email', message: 'The input is not valid E-mail!',},{ required: true, message: 'Please input your E-mail!',
+          },
+        ]}
+      >
         <Input />
       </Form.Item>
-      <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
+
+      <Form.Item
+        name="password"
+        label="Senha"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirmar Senha"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(rule, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject('The two passwords that you entered do not match!');
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      </Form.Item>
+      <Form.Item name="telefone" label="Telefone" rules={[{ type: 'tel' }]}>
+        <Input/>
+      </Form.Item>
+
+      <Form.Item name="cidade" label="Cidade">
         <Input />
       </Form.Item>
-      <Form.Item name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
-        <InputNumber />
-      </Form.Item>
-      <Form.Item name={['user', 'website']} label="Website">
+
+      <Form.Item name="estado" label="Estado">
         <Input />
       </Form.Item>
-      <Form.Item name={['user', 'introduction']} label="Introduction">
+
+      <Form.Item name="site" label="Site da ONG">
+        <Input />
+      </Form.Item>
+      <Form.Item name="descricao" label="Descrição">
         <Input.TextArea />
       </Form.Item>
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
         <Button type="primary" htmlType="submit">
-          Submit
+          Registrar
         </Button>
       </Form.Item>
     </Form>
