68e6a77079a3b9cebcaca8af1569baf90e662c85
gxstav <gustavoscbarcelos@gmail.com>
2020-09-02 20:08:26 -0300

    table ong and migration

--- backend/src/database/migrations/00_create_ong.js
+++ backend/src/database/migrations/00_create_ong.js
@@ -5,6 +5,12 @@ module.exports = {
     return knex.schema.createTable('ong', table => {
       table.increments('id').primary()
       table.string('name').notNullable()
+      table.string('email').notNullable()
+      table.string('phone').notNullable()
+      table.string('city').notNullable()
+      table.string('uf', 2).notNullable()
+      table.string('url').notNullable()
+      table.text('about').notNullable()
     })
   },
   
