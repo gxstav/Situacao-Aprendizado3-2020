2e97511908675b1c6095aea99a0a4395d84fc414
ojedabecka <ojedabecka@outlook.com>
2020-10-19 21:11:26 -0300

    register update

--- frontend/src/pages/Cadastro/index.jsx
+++ frontend/src/pages/Cadastro/index.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import './styles.css';
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
-import { Form, Input, Button, Col } from 'antd';
+import { Form, Input, Button, Col, Row, PageHeader  } from 'antd';
+import herocadastro from '../../assets/images/hero.png'
 import { Layout } from 'antd';
-
-
-const { Content } = Layout;
 
 /*
   const layout = {
@@ -15,25 +13,6 @@ const { Content } = Layout;
 }; 
 
 */
-
-const mystyle = {
-  color: "white",
-  backgroundColor: "DodgerBlue",
-  padding: "10px",
-  fontFamily: "Arial"
-};
-
-const meuregistro = {
-  width: "50%",
-  height: "50%",
-  margin: 0,
-  padding: 0  
-
-};
-
-const formstyle = {
-  color: "white"
-};
 
 /*
 const validateMessages = {
@@ -47,14 +26,10 @@ const validateMessages = {
   },
 };
  */
-
-
 function Registrado(){
   return console.log("REGISTRADO");
 }
-
 function Cadastro(){
-
   async function handleCadastro(event) {
     event.preventDefault()
     try {
@@ -64,117 +39,96 @@ function Cadastro(){
     }
   }
 
-  return (
+return (
     <Layout>
-    <div id="cadastroPage">
-     <Navbar />
-     <p></p>
-     
-     <h1 style={mystyle}>Cadastro</h1>
-     <p></p>
-     
-    <Col span={18} push={6}> 
-    <img src="../logo.jpg" alt="Heroes" width={700}></img>
-    </Col>
-     
-    <Col span={18} push={6}> 
+         <div id="cadastroPage">
+           <Navbar />
+           <div className="site-page-header">
+            <PageHeader onBack={() => null} title="Cadastro"/>
+           </div>
+         <Row>
+         <Col span={12} className="cadastroimage"> 
+           <div >
+             <img src = {herocadastro} alt=""  />
+           </div>
+         </Col>
+         <Col span={12} className="form">
+            <div className="formEdit">
+              <h2>Preencha os campos abaixo:</h2>
+              <Form name="cadastro" onSubmit={handleCadastro}>
+              <Form.Item name="nome">      
+              <Input placeholder='Nome do Responsável'/>    
+               </Form.Item>
+                  <Form.Item name="email" rules={[{ type: 'email', message: 'Este não é um email válido!',},{ required: true, message: 'Por favor insira seu email!',
+                    },
+                  ]}
+                >
+                  <Input placeholder='Email da ONG ou Responsável'/>
+                </Form.Item>
 
-    <b>Preencha os campos abaixo: </b>
-    
-    <p></p>
+                <Form.Item
+                  name="password"        
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Por favor insira sua senha!',
+                    },
+                  ]}
+                  hasFeedback
+                >
+                  <Input.Password placeholder='Senha'/>
+                </Form.Item>
 
-    <Content style={meuregistro}>
-     <Form name="cadastro" onSubmit={handleCadastro}>
-      <Form.Item name="nome" style={formstyle}>      
-        <Input placeholder='Nome do Responsável'/>    
-        </Form.Item>
-        <p></p>
-      
-        <Form.Item
-        name="email" rules={[{ type: 'email', message: 'Este não é um email válido!',},{ required: true, message: 'Por favor insira seu email!',
-          },
-        ]}
-      >
-        <Input placeholder='Email da ONG ou Responsável'/>
-      </Form.Item>
+                <Form.Item name="confirm" dependencies={['password']} hasFeedback rules={[
+                    {
+                      required: true,
+                      message: 'Por favor confirme sua senha!',
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(value) {
+                        if (value || getFieldValue('password') === value) {
+                          return Promise.resolve();
+                        }else{
+                        return Promise.reject('As senhas não correpondem!');}
+                      },
+                    }),
+                  ]}
+                >
+                  <Input.Password placeholder='Confirmar Senha' />
+                </Form.Item>
 
-      <Form.Item
-        name="password"        
-        rules={[
-          {
-            required: true,
-            message: 'Por favor insira sua senha!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password placeholder='Senha'/>
-      </Form.Item>
+                <Form.Item name="telefone" type="number">
+                  <Input placeholder='Telefone'/>
+                </Form.Item>
 
-      <Form.Item
-        name="confirm"       
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Por favor confirme sua senha!',
-          },
-          ({ getFieldValue }) => ({
-            validator(value) {
-              if (value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }else{
-              return Promise.reject('The two passwords that you entered do not match!');}
-            },
-          }),
-        ]}
-      >
-        <Input.Password placeholder='Confirmar Senha' />
-      </Form.Item>
+                <Form.Item name="cidade" rules={[{ required: true }]}>
+                  <Input placeholder='Cidade' />
+                </Form.Item>
 
-      
-      <Form.Item name="telefone" type="number">
-        <Input placeholder='Telefone'/>
-      </Form.Item>
+                <Form.Item name="estado" rules={[{ required: true }]}>
+                  <Input placeholder='Estado'/>
+                </Form.Item>
 
-      <Form.Item name="cidade" rules={[{ required: true }]}>
-        <Input placeholder='Cidade' />
-      </Form.Item>
+                <Form.Item name="site">
+                  <Input placeholder='Site da ONG (opcional)' />
+                </Form.Item>
 
-      <Form.Item name="estado" rules={[{ required: true }]}>
-        <Input placeholder='Estado'/>
-      </Form.Item>
+                <Form.Item name="descricao" label="Descrição" rules={[{ required: true }]}>
+                  <Input.TextArea />
+                </Form.Item>
 
-      <Form.Item name="site">
-        <Input placeholder='Site da ONG (opcional)' />
-      </Form.Item>
-      <Form.Item name="descricao" label="Descrição" rules={[{ required: true }]}>
-        <Input.TextArea />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" onSubmit={Registrado()}>
-          Registrar
-        </Button>
-      </Form.Item> 
-       
-       
-     
-    </Form>   
-    
-    </Content>
-
-
-    </Col>
-  
-     
-        
-        
-     
-    <Footer></Footer>
- 
-    </div>
-  </Layout>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit" onSubmit={Registrado()}>
+                    Registrar
+                  </Button>
+                </Form.Item> 
+              </Form>  
+            </div>
+         </Col>
+         </Row> 
+        <Footer />
+      </div> 
+    </Layout>
   )
 }
 export default Cadastro;
