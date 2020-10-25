6ba0525e9cc3051e5964cabfd2f8586c686ed9e8
gxstav <gustavoscbarcelos@gmail.com>
2020-10-25 16:45:06 -0300

    navbar linking

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Layout, Menu, Button, Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import './style.css';
-import Cadastro from '../../pages/Cadastro';
-import iconheroes from '../../assets/images/iconheroes.png'
+import iconheroes from '../../assets/images/iconheroes.png';
 
 const { Header } = Layout;
 
 
-function navbar(){ 
+function Navbar(){
+
+  const history = useHistory()
+
+  function gotoCadastro() {
+    history.push('/cadastro')
+  }
+
+  function gotoHome() {
+    history.push('/')
+  }
+
+  function gotoAbout() {
+    history.push('/about')
+  }
 
   return(
    
   <Layout id="layout">
     <Header>
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">Início</Menu.Item>
-        <Menu.Item key="2">Conheça os Heroes</Menu.Item>
+        <Menu.Item key="1" onClick={gotoHome}>Início</Menu.Item>
+        <Menu.Item key="2" onClick={gotoAbout}>Conheça os Heroes</Menu.Item>
         <div className="signUp">
-          <img src = { iconheroes } alt=""/>
-          <Button id="botao" type="primary" onClick={Cadastro}>Cadastre-se</Button>
+          <img src={ iconheroes } alt="Heroes"/>
+          <Button id="botao" type="primary" onClick={gotoCadastro}>Cadastre-se</Button>
           <Avatar id="avatar" size={44} icon={<UserOutlined />} />
-          
         </div>
       </Menu>
     </Header>
   </Layout>
   )}
-export default navbar;
+export default Navbar;
