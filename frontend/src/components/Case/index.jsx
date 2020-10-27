becd40a81bcf4c6bc2e6e2c154debabecbe5ba91
ojedabecka <ojedabecka@outlook.com>
2020-10-26 21:30:20 -0300

    case update

--- frontend/src/components/Case/index.jsx
+++ frontend/src/components/Case/index.jsx
@@ -1,30 +1,69 @@
 import React from 'react'
 import './style.css';
-import { Descriptions, Progress, Button  } from 'antd';
-import { RiseOutlined } from '@ant-design/icons'
+import { Descriptions, Progress, Button, Tabs, Statistic, Row, Col, Card  } from 'antd';
+import { RiseOutlined, LikeOutlined} from '@ant-design/icons'
+import banner3 from '../../assets/images/banner3.png'
+const { Countdown } = Statistic;
+const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
+const { TabPane } = Tabs;
 
 function Case (){
+    function onFinish() {
+        console.log('finished!');
+      }
+
+    function callback(key) {
+        console.log(key);
+      }
     return(
     <div id="container">
-          <Descriptions title="NOME DO PROJETO" layout="vertical" bordered>
-            <Descriptions.Item label="ONG">Nome da ONG</Descriptions.Item>
-            <Descriptions.Item label="CAUSA">Motivo da abertura do projeto</Descriptions.Item>
-        <Descriptions.Item label="DESCRIÇÃO DO PROJETO"> Descrição </Descriptions.Item>
-            <Descriptions.Item label="CONTRIBUIÇÃO">Tipo de doação que a ONG solicita</Descriptions.Item>
-            <Descriptions.Item label="CONTATO">Contato da ONG</Descriptions.Item>
-            <Descriptions.Item label="LOCAL"> localização</Descriptions.Item>
-        </Descriptions>
-        <div className="Progress">
-            <h2><RiseOutlined />  Andamento do projeto</h2>
-                <p>doações efetuadas</p>
-                <Progress percent={30} />
-                <p>novos voluntários</p>
-                <Progress percent={50} status="active" />
-        </div>
-        <div className="buttons">
-        <Button type="primary">Quero ajudar</Button>
-        <Button type="primary">Quero me voluntariar</Button> 
-        </div>
+             <div >
+                <img src = {banner3} alt=""  className="bannercase" />
+            </div>
+            <h1 className="titleproject">NOME DO PROJETO</h1>
+            <Tabs defaultActiveKey="1" onChange={callback}>
+                
+                <TabPane tab="SOBRE O PROJETO" key="1" className="tabsTitle">
+                <Descriptions title="User Info" layout="vertical">
+                <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+                <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+                <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
+                <Descriptions.Item label="Address" span={2}>
+                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+                </Descriptions.Item>
+                <Descriptions.Item label="Remark">empty</Descriptions.Item>
+            </Descriptions>
+                </TabPane>
+                <TabPane tab="INFORMAÇÕES DE CONTATO" key="2" className="tabsTitle">
+                <Descriptions title="User Info" layout="vertical">
+                <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+                <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+                <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
+                <Descriptions.Item label="Address" span={2}>
+                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+                </Descriptions.Item>
+                <Descriptions.Item label="Remark">empty</Descriptions.Item>
+            </Descriptions>
+                </TabPane>
+            </Tabs>
+            <div className="CardStatistic">
+                <Row gutter={16}>
+                <Col span={8}>
+                    <Card title="Curtidas" bordered={false}>
+                    <Statistic value={1128} prefix={<LikeOutlined />} />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="O projeto será finalizado em" bordered={false}>
+                         <Countdown value={deadline} onFinish={onFinish} />
+                    </Card>
+                </Col>
+                </Row>
+            </div>
+            <div className="buttons">
+                    <Button type="primary" className="HelpButton">Quero ajudar</Button>
+                    <Button type="primary" className="VoluntaryButton">Quero me voluntariar</Button> 
+            </div>
     </div>
 
     )
